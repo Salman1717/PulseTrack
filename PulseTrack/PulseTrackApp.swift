a346f1ea111817aefcacd3f90d507a5bb476c996
Salman1717 <salmanmhaskar17@gmail.com>
2025-12-30 23:47:31 +0530

    Initial Commit

---
+++ PulseTrack/PulseTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulseTrackApp.swift
+//  PulseTrack
+//
+//  Created by Salman Mhaskar on 30/12/25.
+//
+
+import SwiftUI
+
+@main
+struct PulseTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
